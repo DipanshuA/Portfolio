db5a830f633189c3db4fb46549c6ec8ef8c23c00
DipanshuA <dipanshus841@gmail.com>
2023-10-24 14:25:38 +0530

    m

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,12 +6,12 @@ const Skills = () => {
         {
             id: 1,
             name: "Web Development", description: 'A web developer designs and builds websites and web applications, ensuring functionality and user experience.',
-            image: '/src/assets/skillA.jpg'
+            image: './skillA.jpg'
         },
         {
             id: 2,
             name: "MERN Stack", description: 'Learning MERN involves mastering the essential technologies (MongoDB, Express.js, React, Node.js) for building modern web applications.',
-            image: '/src/assets/skillA.jpg'
+            image: './skillB.jpg'
         },
         {
             id: 3,
